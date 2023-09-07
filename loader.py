29cc4b88ffccb61e155fbaebfcc5a2eae284081b
shawnwangnih <108429233+shawnwangnih@users.noreply.github.com>
2023-09-07 16:25:11 -0400

    comment out removal

--- loader.py
+++ loader.py
@@ -300,12 +300,12 @@ def main(args=None):
                         else:
                             log.error(f'Moving failed file {file} failed! File is still where this code ran and not in fail location.')
                     log.info(f'Attempting to remove failed files from the source {config.s3_bucket}/{config.s3_folder}.')
-                    for file in files:
-                        result = remove_file(config.s3_bucket, config.s3_folder, file)
-                        if result:
-                            log.info(f'Removing fail file {file} succeeded!')
-                        else:
-                            log.error(f'Removing fail file {file} failed! File is still in source location {config.s3_bucket}/{config.s3_folder}.')
+                    #for file in files:
+                        #result = remove_file(config.s3_bucket, config.s3_folder, file)
+                        #if result:
+                        #    log.info(f'Removing fail file {file} succeeded!')
+                        #else:
+                        #    log.error(f'Removing fail file {file} failed! File is still in source location {config.s3_bucket}/{config.s3_folder}.')
                 else:
                     log.info(f'Data files not moved, still where this code ran and in source and not in proper failure destination.')
                     
